c07c9859b77709f0f15fdf75d23164b50fc4a835
vrunoa <vruno.alassia@gmail.com>
2022-05-23 12:47:33 -0700

    add unit test

--- internal/unit/unit_test.go
+++ internal/unit/unit_test.go
@@ -84,5 +84,41 @@ func TestParseSize(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestToBytes(t *testing.T) {
+	type args struct {
+		inputSize    float64
+		inputUnit    Unit
+		expectedSize int64
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "check mega to bytes",
+			args: args{
+				inputSize:    1.0,
+				inputUnit:    mega,
+				expectedSize: 1036288,
+			},
+		},
+		{
+			name: "check giga to bytes",
+			args: args{
+				inputSize:    1.0,
+				inputUnit:    giga,
+				expectedSize: 1061158912,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := ToBytes(tt.args.inputSize, tt.args.inputUnit)
+			if res != tt.args.expectedSize {
+				t.Errorf("wrong conversion. Want: %v Got: %v", tt.args.expectedSize, res)
+			}
+		})
+	}
 }
